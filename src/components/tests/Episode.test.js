a8163f76e9f389177c1d8a48fd7c3681cdc451f3
prodhc <zionywood@gmail.com>
2021-05-17 20:39:25 -0700

    mvp

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,30 +3,43 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 const testEpisode = {
-    id:1,
-    name: "",
-    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
-    season: 1,
-    number: 1,
-    summary: "",
-    runtime: 1
-}
+	id: 1,
+	name: '',
+	image: 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg',
+	season: 1,
+	number: 1,
+	summary: 'Hi how are ya',
+	runtime: 1
+};
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
-}
+	//Add in appropriate test data structure here.
+	...testEpisode,
+	image: null
+};
 
-test("renders without error", () => {
-
+test('renders without error', () => {
+	render(<Episode episode={testEpisode} />);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test('renders the summary test passed as prop', () => {
+	render(<Episode episode={testEpisode} />);
+
+	const summary = screen.queryByText(/Hi how are ya/i);
+
+	expect(summary).toBeInTheDocument();
+	expect(summary).toBeTruthy();
+	expect(summary).toHaveTextContent(/Hi how are ya/i);
+	expect(summary).not.toBeNull();
 });
 
-test("renders default image when image is not defined", ()=>{
-    
-})
+test('renders default image when image is not defined', () => {
+	render(<Episode episode={testEpisodeWithoutImage} />);
+
+	const imageAlt = screen.queryByAltText('./stranger_things.png');
+
+	expect(imageAlt).toBeTruthy();
+});
 
 //Tasks
 //1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
